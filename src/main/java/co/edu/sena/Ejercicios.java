9d86655bb3a9e907286970883af027ac7956a99b
David <davidm435465123@gmail.com>
2024-03-03 07:45:39 -0500

    Se creo el archivo Ejercicios.java y se realizo el primer reto de operadores

---
+++ src/main/java/co/edu/sena/Ejercicios.java
@@ -0,0 +1,9 @@
+package co.edu.sena;
+
+public class Ejercicios {
+
+    public double areaTriangulo(double d1, double d2){
+        return (d1 * d1) / 2;
+    }
+}
+    
---
+++ src/main/java/co/edu/sena/Main.java
@@ -0,0 +1,27 @@
+package co.edu.sena;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double dato1;
+        double dato2;
+        double dato3;
+        double dato4;
+
+        // Todo Menú y Submenús
+        Ejercicios ejercicios = new Ejercicios();
+
+        System.err.println("1.1 Calcular el área de un triángulo");
+        Scanner scr = new Scanner(System.in);
+
+        System.out.println("Ingrese la base del triángulo: ");
+        dato1 = scr.nextDouble();
+        System.out.println("Ingrese la altura del triángulo: ");
+        dato2 = scr.nextDouble();
+        dato3 = ejercicios.areaTriangulo(dato1, dato2);
+        System.out.println("El area del triángulo es: " + dato3);
+
+    }
+
+}
