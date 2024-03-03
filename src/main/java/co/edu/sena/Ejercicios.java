7e203613c3e2bab31fc91cada8fb21de0b532fa2
David <davidm435465123@gmail.com>
2024-03-03 07:51:56 -0500

    Se realizo el segundo reto

--- src/main/java/co/edu/sena/Ejercicios.java
+++ src/main/java/co/edu/sena/Ejercicios.java
@@ -5,5 +5,8 @@ public class Ejercicios {
     public double areaTriangulo(double d1, double d2){
         return (d1 * d1) / 2;
     }
+    public double sumarNumeros(double d1, double d2){
+        return (d1 + d2);
+    }
 }
     
--- src/main/java/co/edu/sena/Main.java
+++ src/main/java/co/edu/sena/Main.java
@@ -25,3 +25,25 @@ public class Main {
     }
 
 }
+
+class Sumnumbers {
+    public static void main(String[] args) {
+        double dato1;
+        double dato2;
+        double dato3;
+        double dato4;
+
+        // Todo Menú y Submenús
+        Ejercicios ejercicios = new Ejercicios();
+
+        System.err.println("1.2 Ingresar 2 numeros y sumarlos");
+        Scanner scr = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número: ");
+        dato1 = scr.nextDouble();
+        System.out.println("Ingrese el segundo número: ");
+        dato2 = scr.nextDouble();
+        dato3 = ejercicios.sumarNumeros(dato1, dato2);
+        System.out.println("La suma es: " + dato3);
+    }
+}
